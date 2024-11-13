89a53ddd3a1388be2df4e7d4f89c9c1ef0bc9c5d
IkuroMaira <gwen.bussac@gmail.com>
2024-11-13 11:18:53 +0100

    Add conditions and store in a variable

--- 07_jeu_des_allumettes/script_02.js
+++ 07_jeu_des_allumettes/script_02.js
@@ -2,24 +2,25 @@
 
 // DOM :
 const input = document.querySelector('#input-number');
-// console.log(input);
 const btn = document.querySelector('#btn-validate');
-// console.log(btn);
 const divMessage = document. querySelector('.div-message');
 
 let givenNumberMatches = null;
+let matchesToRemove = 10;
+let numberOfMatchesToDelete = null;
 
+// Lorsque je clique sur le bouton valider
 // Je récupère la valeur de l'input par un écouteur d'évènement sur le bouton valider
 btn.addEventListener('click', () =>
 {
-    givenNumberMatches = input.value;
+    givenNumberMatches = parseInt(input.value);
+    // console.log(givenNumberMatches);
     console.log(`Valeur validéé: ${givenNumberMatches}`);
 
-    console.log(checkNumber(givenNumberMatches));
+    checkNumber(givenNumberMatches);
 })
 
 // Je veux vérifier que la valeur est bien ente 1 et 6
-
 checkNumber = (number) => {
     let message = document.createElement('p');
     if (number >= 1 && number<= 6) {
@@ -28,11 +29,19 @@ checkNumber = (number) => {
         // Affichage du message :
         message.textContent = 'Le nombre est entre 1 et 6.';
         divMessage.appendChild(message);
+
+        // Si les conditions sont remplies, je stocke dans une variable :
+        numberOfMatchesToDelete += number;
+        console.log(`La valeur stockée: ${numberOfMatchesToDelete}`);
     } else {
         console.log('Veuillez entrer un nombre entre 1 et 6.');
 
         // Affichage du message :
         message.textContent = 'Veuillez entrer un nombre entre 1 et 6.';
         divMessage.appendChild(message);
+
+        // Si les condition ne sont pas remplies, je ne remplie pas la variable.
+        // Je ne mets pas la variable à zéro pour ne pas perdre les valeurs entrée précédemment.
+        console.log(`La valeur stockée: ${numberOfMatchesToDelete}`);
     }
 }
