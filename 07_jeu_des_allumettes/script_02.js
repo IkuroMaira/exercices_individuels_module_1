5cb11cb1b7716b8b9e30652000bbbbb91c708b8f
IkuroMaira <gwen.bussac@gmail.com>
2024-11-13 10:42:34 +0100

    Check given number

--- 07_jeu_des_allumettes/script_02.js
+++ 07_jeu_des_allumettes/script_02.js
@@ -5,10 +5,34 @@ const input = document.querySelector('#input-number');
 // console.log(input);
 const btn = document.querySelector('#btn-validate');
 // console.log(btn);
+const divMessage = document. querySelector('.div-message');
+
+let givenNumberMatches = null;
 
 // Je récupère la valeur de l'input par un écouteur d'évènement sur le bouton valider
 btn.addEventListener('click', () =>
 {
-    let givenNumberMatches = input.value;
+    givenNumberMatches = input.value;
     console.log(`Valeur validéé: ${givenNumberMatches}`);
+
+    console.log(checkNumber(givenNumberMatches));
 })
+
+// Je veux vérifier que la valeur est bien ente 1 et 6
+
+checkNumber = (number) => {
+    let message = document.createElement('p');
+    if (number >= 1 && number<= 6) {
+        console.log('Le nombre est entre 1 et 6.');
+
+        // Affichage du message :
+        message.textContent = 'Le nombre est entre 1 et 6.';
+        divMessage.appendChild(message);
+    } else {
+        console.log('Veuillez entrer un nombre entre 1 et 6.');
+
+        // Affichage du message :
+        message.textContent = 'Veuillez entrer un nombre entre 1 et 6.';
+        divMessage.appendChild(message);
+    }
+}
