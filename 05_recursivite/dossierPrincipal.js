ceb7165a22f792e5e9630dd81a868aac0dcc525e
Gwenaëlle Bussac <gwen.bussac@gmail.com>
2025-01-23 18:00:32 +0100

    Step 2: done

---
+++ 05_recursivite/dossierPrincipal.js
@@ -0,0 +1,38 @@
+export const dossierPrincipal = {
+    nom: 'Ada',
+    contenu: [{
+        nom: 'Projets collectifs Ada',
+        contenu: [{
+            nom: 'Pico8',
+            contenu: [{
+                nom: 'mariokart.p8'
+            }]
+        },
+            {
+                nom: 'Dataviz',
+                contenu: [{
+                    nom: 'index.html'
+                },
+                    {
+                        nom: 'script.js'
+                    }
+                ]
+            }
+        ]
+    },
+        {nom: 'CV.pdf'},
+        {
+            nom: 'Projets persos',
+            contenu: [{
+                nom: 'Portfolio',
+                contenu: [{
+                    nom: 'index.html'
+                },
+                    {
+                        nom: 'script.js'
+                    }
+                ]
+            }]
+        },
+    ],
+}
