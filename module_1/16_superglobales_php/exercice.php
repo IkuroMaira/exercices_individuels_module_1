0bb1072883f6ec5913da0fc8c1c7b42f3568114e
IkuroMaira <gwen.bussac@gmail.com>
2025-02-21 11:35:05 +0100

    Step 2 finished

--- module_1/16_superglobales_php/exercice.php
+++ module_1/16_superglobales_php/exercice.php
@@ -1,5 +1,15 @@
 <html>
-Bonjour <?php echo $_GET["first_name"] != null ? $_GET['first_name'] : "Anonyme"; ?> <br>
+Bonjour
+<?php
+    if (!empty($_GET["first_name"])) {
+        echo $_GET['first_name'];
+    } elseif (isset($_POST["first_name"])) {
+        echo $_POST["first_name"];
+    } else {
+        echo "Anonyme";
+    }
+?> <br>
+<br>
 <form action="exercice.php" method="post">
     <p>Votre nom : <input type="text" name="first_name" /></p>
     <p><input type="submit" value="OK"></p>
